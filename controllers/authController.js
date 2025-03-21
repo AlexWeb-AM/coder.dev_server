c38a158b546d7f26caf677ae505c68e087d3040f
Alex <balyanalex272@gmail.com>
2025-03-21 18:08:35 +0400

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,11 @@ import connectDB from "../config/connectDB.js";
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import generateToken from "../utils/generateToken.js";
+import transporter from "../config/nodemailer.js";
 
 connectDB();
 
-export const Register = async (req, res) => {
+export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!email || !password || !name) {
@@ -50,16 +51,16 @@ export const Register = async (req, res) => {
       message: "User registered successfully",
       user: savedUser,
     });
-  } catch (error) {
-    console.log("Error:", error);
+  } catch (err) {
+    console.log("Error:", err);
     return res.status(500).json({
       success: false,
-      message: error.message || "Internal server error",
+      message: err.message || "Internal server error",
     });
   }
 };
 
-export const Login = async (req, res) => {
+export const login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -100,10 +101,89 @@ export const Login = async (req, res) => {
       });
       
 
-  } catch (error) {
+  } catch (err) {
     return res.status(500).json({
       success: false,
-      message: error.message || "Internal server error",
+      message: err.message || "Internal server error",
     });
   }
 };
+
+export const sendOtp = async (req,res) => {
+  const {email} = req.body
+
+  if (!email) {
+    return res.status(400).json({success:false,message:"Email are required"})
+  }
+
+
+  try {
+     const user = await userModel.findOne({email})
+
+    if (!user) {
+      return res.status(400).json({success:false,message:"User is not defined"})
+    }
+    
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    user.verifyOtp = otp
+
+    await user.save()
+
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: "Verification Code :)",
+      text: `Your Verification Code ${otp}`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("OTP sent successfully");
+      return res.status(200).json({success:true,message:"OTP sent successfully"})
+    } catch (error) {
+      console.log("Error sending OTP:", error);
+      return res.status(500).json({ success: false, message: "Failed to send OTP" });
+    }
+  
+  } catch (err) {
+    console.error(err)
+    return res.status(400).json({success:false,message:err})
+  }
+
+}
+
+export const checkOtp = async (req,res) => {
+  const {email,otp} = req.body
+  
+  if (!email || !otp)  {
+    return res.status(400).json({success:false,message:"Data are required"})
+  }
+
+  try {
+    const user = await userModel.findOne({email})
+
+    if (!user) {
+      return res.status(400).json({sucess:false,message:"User is not defined"})
+    }
+
+    if (user.verifyOtp === otp) {
+      user.verifyOtp = ''
+      await user.save('')
+
+      return res.status(400).json({success:true,message:"Otp Checked"})
+
+    } else {  
+      return res.status(400).json({success:false,message:"Wrong Otp"})
+    }
+
+  } catch (err) {
+    console.error(err)
+    return res.status(400).json({success:false,message:err})
+  }
+
+}
+
+export const resetPassword = (req,res) => {
+
+}
--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,8 @@ const userScheme = new mongoose.Schema({
     name:{type:String,require:true},
     email:{type:String,require:true,unique:true},
     password:{type:String,require:true},
-    routeId:{type:String,required:true}
+    routeId:{type:String,required:true},
+    verifyOtp:{type:String}
 })
 
 const userModel = mongoose.model('user',userScheme)
